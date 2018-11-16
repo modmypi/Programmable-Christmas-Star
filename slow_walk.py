8926e54b437bac5f79f1261a21f6acc873003896
Claire Pollard <claire.r.pollard@gmail.com>
2018-11-16 16:29:11 +0000

    Update slow_walk.py

--- slow_walk.py
+++ slow_walk.py
@@ -4,17 +4,23 @@ from star import Star
 step = 0.5
 count = 0
 leds = star.leds
-while True:
-    if(count%26!=0):
-        leds[count%26].on()
-        sleep(step)
-        leds[count%26].off()
+
+try:
+    
+    while True:
+        if(count%26!=0):
+            leds[count%26].on()
+            sleep(step)
+            leds[count%26].off()
             
-    count += 1
-    step = step*0.99
+        count += 1
+        step = step*0.99
         
-    if(step <= 0.0001):
-        star.inner.blink(on_time=0.5,off_time=0.5,n=5)
-        sleep(5)
-        count = 0
-        step = 2
+        if(step <= 0.0001):
+            star.inner.blink(on_time=0.5,off_time=0.5,n=5)
+            sleep(5)
+            count = 0
+            step = 2
+
+except KeyboardInterrupt:
+    star.close()
