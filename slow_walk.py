e966019671feab80957415bebbcc2271cfd767bf
Claire Pollard <claire.r.pollard@gmail.com>
2018-11-16 16:27:52 +0000

    Create slow_walk.py

---
+++ slow_walk.py
@@ -0,0 +1,20 @@
+from time import sleep
+from star import Star
+    
+step = 0.5
+count = 0
+leds = star.leds
+while True:
+    if(count%26!=0):
+        leds[count%26].on()
+        sleep(step)
+        leds[count%26].off()
+            
+    count += 1
+    step = step*0.99
+        
+    if(step <= 0.0001):
+        star.inner.blink(on_time=0.5,off_time=0.5,n=5)
+        sleep(5)
+        count = 0
+        step = 2
